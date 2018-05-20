d82cf61d309f8140e3989fab5a2d46b383b37498
Neelesh Rastogi <neelr1997@users.noreply.github.com>
2018-05-20 00:31:56 -0400

    ship motion module - pitch / Roll without sensors

---
+++ motion.py
@@ -0,0 +1,160 @@
+# Module - Calculate roll and pitch of a vessel.
+# Project approach: 1.0
+# Master Project: ship_model.py
+# Base Project: motion.py
+# Python Version: 2.7.12
+
+from math import sin, tan, cos, exp
+import motion_module as module
+import matplotlib.pyplot as plt
+import sys, os, csv
+
+'''
+Index of Variables/Terms/Constants used throughout the module and base project:
+------------------------------------------------------------------------------------------------------------------------------
+# GZ 								| The Righting arm. 													
+# BM 								| Buoyant Force.
+# GM 								| Metacentric height.
+------------------------------------------------------------------------------------------------------------------------------
+# const_roll 						| Constant value of proportionality in the theta calculation for roll   
+# const_pitch 						| Constant value of proportionality in the theta calculation for pitch  
+# const_density 					| Density of the saline sea water. 										           | 1.025
+# const_pi 							| Value of geometric constant 'PI'. 		 							           | 22/7
+------------------------------------------------------------------------------------------------------------------------------
+# wind_force, wf 					| Force of the Wind in beaufort Scale value.
+# w_dir 							| Direction of te wind.
+# wind_direction, wind_angle 		| Angle of inclination of the w_dir vector w.r.t. ship in degree.
+------------------------------------------------------------------------------------------------------------------------------
+# wv_direction, wv_dir, wvd 		| Direction of wave.
+# wave_direction, wave_angle 		| Direction of wave w.r.t. ship in degree.
+# wave_height						| Amplitude of the wave.
+# per 								| Duration of the wave in units 'seconds'.
+------------------------------------------------------------------------------------------------------------------------------
+# Displacement 						| Displacement of the water due to ship's weight in cubic meter.
+# mass 								| Mass of the ship based on volumetric density displaced and saline water density.
+# ship_course, sc 					| Calculated ship course depending on the longitude and latitude.
+# area 								| Ship's estimated surface area.
+------------------------------------------------------------------------------------------------------------------------------
+# kneel_angle 						| angle of deviation of the buoyent force from the metacentric height.
+------------------------------------------------------------------------------------------------------------------------------
+'''
+
+# Hard-code of variables per ship dimensions inclusive of universal constants.
+GM = 1.5
+area = 10213.29
+displacement =  127355
+const_pi = 22/7
+const_density = 1.025
+
+# Constants geberated by module file.
+const_pi = 22/7
+mass = module.Mass(const_density, displacement)
+const_roll, const_pitch = module.regression()
+
+# Function to calculate Kneel angle for pitch.
+def theta_pitch(wind_speed, w_dir, sc, wave_height, wv_dir, per):
+	wind_direction = module.calc_windangle(w_dir, sc)*(const_pi/180)  
+	wave_direction = module.calc_waveangle(wv_dir, sc)*(const_pi/180)
+	spectrum = module.jonswap(wave_height, per)
+	kneel_angle = (const_pitch*cos(spectrum)*cos(wave_direction)/mass*module.beaufort(wind_speed)*cos(wind_direction))
+	return kneel_angle
+
+# Function to calculate Kneel angle for roll.
+def theta_roll(wind_speed, w_dir, sc, wave_height, wv_dir, per):
+	wind_direction = module.calc_windangle(w_dir, sc)*(const_pi/180)  
+	wave_direction = module.calc_waveangle(wv_dir, sc)*(const_pi/180)
+	spectrum = module.jonswap(wave_height, per)
+	kneel_angle = (const_roll*cos(spectrum)*cos(wave_direction)/mass*module.beaufort(wind_speed)*cos(wind_direction))
+	return kneel_angle
+
+# Function to calculate pitch.
+def calc_pitch(wind_speed, w_dir, sc, wave_height, wv_dir, per):
+	angle = theta_pitch(wind_speed, w_dir, sc, wave_height, wv_dir, per)
+	if(angle < 10.0):
+		GZ = GM * sin(angle)
+	else:
+		GZ = sin(GM + 0.5*module.BM(area, displacement)*(tan(angle)**2))
+	return abs(GZ)
+
+# Function to calculate roll.
+def calc_roll(wind_speed, w_dir, sc, wave_height, wv_dir, per):
+	angle = theta_roll(wind_speed, w_dir, sc, wave_height, wv_dir, per)
+	if(angle < 10.0):
+		GZ = GM * sin(angle)
+	else:
+		GZ = sin(GM + 0.5*module.BM(area, displacement)*(tan(angle)**2))
+	return abs(GZ)
+
+# Function to call specific roll/pitch function based on wind and wave direction.
+def calc_value(wind_speed, w_dir, sc, wave_height, wv_dir, per):
+	wind_angle = module.calc_windangle(w_dir, sc)
+	wave_angle = module.calc_waveangle(wv_dir, sc)
+	wind_force = module.beaufort(wind_speed)
+	
+	if wind_angle < 0 or wave_angle < 0:
+		wind_angle = wind_angle + 180
+		wave_angle = wave_angle + 180
+	elif wind_angle > 180 or wave_angle > 180:
+		wind_angle = wind_angle - 180
+		wave_angle = wave_angle - 180
+
+	pitch = 0.0
+	roll = 0.0
+
+	if per == 0:
+		return pitch , roll
+	else: 
+		pitch = calc_pitch(wind_force, w_dir, sc, wave_height, wv_dir, per)
+		roll = calc_roll(wind_force, w_dir, sc, wave_height, wv_dir, per)
+	# Swap for cross verification of roll always bigger than pitch.
+		if (pitch>roll):
+			temp = roll
+			roll = pitch
+			pitch = temp
+	
+	if (wind_angle >= 0) and (wind_angle < 60) or (wind_angle >= 120) and (wind_angle < 180) or (wind_angle >= 60) and (wind_angle < 120):
+		return pitch , roll
+	else:
+		return pitch , roll
+
+'''---------------------------------------------------------END OF MODULE--------------------------------------------------'''
+
+# Module Tester Function.
+'''
+--------------------------------------------------------
+ Input Value Order:   
+ 				1. 	Wind Speed
+ 				2. 	Wind Direction 
+ 				3. 	Ship Course (cog)
+ 				4. 	Wave Height
+ 				5.	Wave Direction
+ 				6. 	Tidal Time (per)
+---------------------------------------------------------	
+'''
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__file__ = 'Data\Seavoyager Kozmino -Dickson_Recommended_2016-08-01-0930Z_2016-08-01-0930.csv'
+result = []
+def read():
+	with open (os.path.join( __location__ , __file__), 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			value = []
+		#Fixing white spacing error in CSV Extracted Data.
+			row[0] =row[0].replace('\xa0',' ')
+			row[2] =row[2].replace('\xa0',' ')
+			row[6] =row[6].replace('\xa0',' ')
+			row[5] =row[5].replace('\xa0',' ')
+			
+			result.append(calc_value(float(row[0].strip()),  float(row[1].strip()), float(row[7].strip()), float(row[2].strip()), float(row[4].strip()), float(row[3].strip())))
+			
+read()
+pitch = []
+roll = []
+for elem in result:
+	print elem
+	pitch.append(elem[0])
+	roll.append(elem[1])
+
+plt.plot(pitch)
+plt.plot(roll)
+plt.show()
---
+++ motion_module.py
@@ -0,0 +1,167 @@
+# Module - Calculate the Theta Constant of Proportionality based on historic voyage data.
+# Module version: 1.0
+# Python Version: 2.7.12
+
+from math import asin, exp, cos
+import numpy as np
+import sys, os, csv
+import matplotlib.pyplot as plt
+
+'''
+	Data File Name (__file__)		: 	Seavoyager Kozmino -Dickson_Recommended_2016-08-01-0930Z_2016-08-01-0930.csv
+	Extension						: 	Comma Seprated Value (CSV)
+	Data Version Type 				: 	Extracted data. 
+		
+					  					FILE INDEX	|		  VALUE NAME 		|	VALUE TYPE
+									-------------------------------------------------------------
+										Row[0] 		| 		wind_speed			| 		str 
+										Row[1] 		| 		wind direction 		| 		str
+										Row[2] 		| 		Wave height 		| 		str
+										Row[3] 		| 		per 				| 		str
+										Row[4] 		| 		wave direction 		| 		str
+										Row[5] 		| 		roll 				| 		str
+										Row[6] 		| 		pitch 				| 		str
+										Row[7] 		| 		COG 				| 		str
+									-------------------------------------------------------------
+
+
+	*Note: for Variables/Terms/Constants Index refer main frame file.
+	
+'''
+
+# Hard-coding of variables per ship dimensions and universal constants.
+GM = 1.5
+area = 10213.29
+displacement = 127355
+const_pi = 22/7
+const_density = 1.025
+
+# Declaration of current and data file path within the module.
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__file__ = 'Data\Seavoyager Kozmino -Dickson_Recommended_2016-08-01-0930Z_2016-08-01-0930.csv'
+
+# Global Array varibales to store all the roll and pitch constants as a list.
+roll_constant = []
+pitch_constant = []
+
+# Function to calculate Ship's Mass.
+def Mass(const_density,displacement):
+	return const_density*displacement
+
+#Function to calculate Buoyant force.
+def BM(area, displacement):
+	return area/displacement
+
+# Function to calculate the wave force based on wave spectrum.
+def jonswap(wave_height, per):
+	cir_freq = 2*const_pi/per
+	Y = 3.3
+	spectrum = ((320*(wave_height**2))/(per**4))*(cir_freq**(-5))*(exp((-1950/(per**4))*(cir_freq**(-4))))*(Y**4)
+	return spectrum
+
+# Function to calculate wind angle w.r.t Ship's course.
+def calc_windangle(w_dir, ship_course):
+	if (w_dir - ship_course > 180):
+		wd = w_dir - ship_course - 360
+	if (w_dir - ship_course < -180):
+		wd = w_dir - ship_course + 360
+	else:
+		wd = w_dir - ship_course
+	return wd
+
+# Function to calculate wave angle w.r.t Ship's course.
+def calc_waveangle(wv_direction, ship_course):
+	if (wv_direction - ship_course > 180):
+		wvd  = wv_direction - ship_course - 360
+	if (wv_direction - ship_course < -180):
+		wvd = wv_direction - ship_course + 360
+	else:
+		wvd = wv_direction - ship_course
+	return wvd
+
+# Function to convert wind speed in Wind Force.
+def beaufort(wind_speed):
+	if wind_speed<1.0:
+		force = 0.0
+		return force
+	elif wind_speed>=1.0 and wind_speed<=3.0:
+		force = 1.0
+		return force
+	elif wind_speed>=4.0 and wind_speed<=6.0:
+		force = 2.0
+		return force
+	elif wind_speed>=7.0 and wind_speed<=10.0:
+		force =  3.0
+		return force
+	elif wind_speed>=11.0 and wind_speed<=16.0:
+		force =  4.0
+		return force
+	elif wind_speed>=17.0 and wind_speed<=21.0:
+		force =  5.0
+		return force
+	elif wind_speed>=22.0 and wind_speed<=27.0:
+		force =  6.0
+		return force
+	elif wind_speed>=28.0 and wind_speed<=33.0:
+		force =  7.0
+		return force
+	elif wind_speed>=34.0 and wind_speed<=40.0:
+		force =  8.0
+		return force
+	elif wind_speed>=41.0 and wind_speed<=47.0:
+		force =  9.0
+		return force
+	elif wind_speed>=48.0 and wind_speed<=55.0:
+		force = 10.0
+		return force
+	elif wind_speed>=56.0 and wind_speed<=63.0:
+		force = 11.0
+		return force
+	else:
+		force = 12.0
+		return force
+
+# Function to calculate the roll proportionality constant.
+def rollc(GZ_roll, GM, M, wind_speed, wd, cog, wvd, spectrum):
+	return (asin(GZ_roll/GM)*M)*beaufort(wind_speed)*cos(calc_windangle(wd, cog))/cos(spectrum)*cos(calc_waveangle(wvd, cog))
+
+# Function to calculate the pitch proportionality constant.
+def pitchc(GZ_pitch, GM, M, wind_speed, wd, cog, wvd, spectrum):
+	return (asin(GZ_pitch/GM)*M)*beaufort(wind_speed)*cos(calc_windangle(wd, cog))/cos(spectrum)*cos(calc_waveangle(wvd, cog))
+
+# Function to assign values to respective variables and call functions rollc(...) and pitchc(...).
+def calc_assign_values(GZ_roll, GZ_pitch, height, per, wd, wvd, cog, wind_speed):
+	M = Mass(const_density,displacement)
+	if per==0:
+		return roll_constant.append(0.0), pitch_constant.append(0.0)
+	spectrum = jonswap(height, per)
+	
+	roll_constant.append(rollc(GZ_roll, GM, M, wind_speed, wd, cog, wvd, spectrum))
+	pitch_constant.append(pitchc(GZ_pitch, GM, M, wind_speed, wd, cog, wvd, spectrum))
+
+# Function to read the data file and pass it on to function calc_assign_values(...).
+def read_csv():
+	result = []
+	with open (os.path.join( __location__ , __file__), 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+		#Fixing white spacing error in CSV Extracted Data.
+			row[0] =row[0].replace('\xa0',' ')
+			row[1] =row[1].replace('\xa0',' ')
+			row[2] =row[2].replace('\xa0',' ')
+			row[3] =row[3].replace('\xa0',' ')
+			row[4] =row[4].replace('\xa0',' ')
+			row[5] =row[5].replace('\xa0',' ')
+			row[6] =row[6].replace('\xa0',' ')
+			row[7] =row[7].replace('\xa0',' ')
+			result.append(calc_assign_values(float(row[5].strip()), float(row[6].strip()), float(row[2].strip()),float(row[3].strip()), float(row[4].strip()), float(row[1].strip()), float(row[7].strip()), float(row[0].strip())))
+			
+
+# Function to calculate and return the regression value of both pitch and roll's constant values.
+def regression():
+	read_csv()
+	constant_roll = np.mean(roll_constant)
+	constant_pitch = np.mean(pitch_constant)
+	return constant_roll, constant_pitch
+
+'''---------------------------------------------------------END OF MODULE--------------------------------------------------'''
